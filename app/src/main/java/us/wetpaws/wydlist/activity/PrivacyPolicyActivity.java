d337a3adc36ae8ac05e376d66c66c45daa628cc9
Harrison <tranharrywich@gmail.com>
2017-04-08 10:16:08 -0700

    Created home, mylist, and adventure fragments blanks. Created privacy policy and term and condition activity blanks.

---
+++ app/src/main/java/us/wetpaws/wydlist/activity/PrivacyPolicyActivity.java
@@ -0,0 +1,15 @@
+package us.wetpaws.wydlist.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import us.wetpaws.wydlist.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
---
+++ app/src/main/java/us/wetpaws/wydlist/activity/TermAndConditionActivity.java
@@ -0,0 +1,15 @@
+package us.wetpaws.wydlist.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import us.wetpaws.wydlist.R;
+
+public class TermAndConditionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_and_condition);
+    }
+}
