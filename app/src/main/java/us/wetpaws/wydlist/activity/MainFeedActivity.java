46fceb37954f50023a2fac29c162575826b6be0b
Harrison <tranharrywich@gmail.com>
2017-04-08 11:12:52 -0700

    Created main feed activity and added the navigation drawer and the navigation header.

---
+++ app/src/main/java/us/wetpaws/wydlist/activity/MainFeedActivity.java
@@ -0,0 +1,15 @@
+package us.wetpaws.wydlist.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import us.wetpaws.wydlist.R;
+
+public class MainFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_feed);
+    }
+}
