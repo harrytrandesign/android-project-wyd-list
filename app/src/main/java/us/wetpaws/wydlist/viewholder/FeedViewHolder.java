d663a18d177e9fab19b92d1331977a4146963b53
Harrison <tranharrywich@gmail.com>
2017-07-24 13:42:36 -0700

    FeedViewHolder added the finditembyid labels, need the setters.

--- app/src/main/java/us/wetpaws/wydlist/viewholder/FeedViewHolder.java
+++ app/src/main/java/us/wetpaws/wydlist/viewholder/FeedViewHolder.java
@@ -2,6 +2,10 @@ package us.wetpaws.wydlist.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import us.wetpaws.wydlist.R;
 
 /**
  * Created by HTDWPS on 7/24/17.
@@ -9,11 +13,18 @@ import android.view.View;
 
 public class FeedViewHolder extends RecyclerView.ViewHolder {
 
+    private ImageView bucketlist_bg_image;
+    private TextView bucketlist_post_title;
+    private TextView bucketlist_post_timestamp;
+
     public FeedViewHolder(View itemView) {
         super(itemView);
+
+        bucketlist_bg_image = (ImageView) itemView.findViewById(R.id.bucketlist_image);
+        bucketlist_post_title = (TextView) itemView.findViewById(R.id.bucketlist_title_text);
+        bucketlist_post_timestamp = (TextView) itemView.findViewById(R.id.bucketlist_date_timestamp);
     }
 
-
-
+    // TODO: Create the setter methods.
 
 }
