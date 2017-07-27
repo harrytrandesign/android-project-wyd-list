b7a26991b0f1ca2113d5f7472b4ba647b5516391
Harrison <tranharrywich@gmail.com>
2017-07-26 22:27:42 -0700

    Finished up Personal View Holder and the layout for single item.

--- app/src/main/java/us/wetpaws/wydlist/viewholder/PrivateViewHolder.java
+++ app/src/main/java/us/wetpaws/wydlist/viewholder/PrivateViewHolder.java
@@ -2,6 +2,9 @@ package us.wetpaws.wydlist.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import us.wetpaws.wydlist.R;
 
 /**
  * Created by HTDWPS on 7/24/17.
@@ -9,12 +12,16 @@ import android.view.View;
 
 public class PrivateViewHolder extends RecyclerView.ViewHolder {
 
+    private TextView personal_bucketlist_title;
+
     public PrivateViewHolder(View itemView) {
         super(itemView);
+
+        personal_bucketlist_title = (TextView) itemView.findViewById(R.id.profile_bucket_title);
     }
 
-
-
-
+    public void setPersonal_bucketlist_title(String text) {
+        personal_bucketlist_title.setText(text);
+    }
 
 }
