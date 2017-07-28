f8b5061e35acc7c7984104d0d45a3455ecf9c88d
Harrison <tranharrywich@gmail.com>
2017-07-28 00:09:19 -0700

    Need to figure out how to Fragment to Intent another Fragment.

--- app/src/main/java/us/wetpaws/wydlist/fragment/HomeFragment.java
+++ app/src/main/java/us/wetpaws/wydlist/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package us.wetpaws.wydlist.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,6 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
 import us.wetpaws.wydlist.R;
+import us.wetpaws.wydlist.activity.CommentActivity;
 import us.wetpaws.wydlist.adapter.FirebaseUtil;
 import us.wetpaws.wydlist.adapter.SimpleDividerItemDecoration;
 import us.wetpaws.wydlist.model.BucketList;
@@ -91,11 +93,19 @@ public class HomeFragment extends Fragment {
         ) {
             @Override
             protected void populateViewHolder(FeedViewHolder viewHolder, BucketList model, int position) {
-                String feedPostKey = ((FirebaseRecyclerAdapter) mListAdapter).getRef(position).getKey();
+                final String feedPostKey = ((FirebaseRecyclerAdapter) mListAdapter).getRef(position).getKey();
 
                 viewHolder.setBucketTitle(model.getTitle());
                 viewHolder.setBackgroundImage(model.getImageurl());
                 viewHolder.setBucketDateTimestamp(DateUtils.getRelativeTimeSpanString((long) model.getTimestamp()).toString());
+                viewHolder.setPostClickListener(new FeedViewHolder.PostClickListener() {
+                    @Override
+                    public void showComments() {
+                        Intent intent = new Intent(HomeFragment.this, CommentActivity.class);
+                        intent.putExtra(CommentActivity.POST_KEY_EXTRA, feedPostKey);
+                        startActivity(intent);
+                    }
+                });
             }
         };
 
