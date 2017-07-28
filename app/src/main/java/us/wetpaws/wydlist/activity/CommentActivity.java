d15f8bbe978546568ab3539134196f2abd82f161
Harrison <tranharrywich@gmail.com>
2017-07-27 19:15:37 -0700

    Wrote up CommentsActivity and also the layout with only framelayout.

---
+++ app/src/main/java/us/wetpaws/wydlist/activity/CommentActivity.java
@@ -0,0 +1,26 @@
+package us.wetpaws.wydlist.activity;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import us.wetpaws.wydlist.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    public static final String POST_KEY_EXTRA = "post_key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+        String postKey = getIntent().getStringExtra(POST_KEY_EXTRA);
+        if (postKey == null) {
+            finish();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.comments_fragment, CommentsFragment.newInstance(postKey)).commit();
+    }
+}
