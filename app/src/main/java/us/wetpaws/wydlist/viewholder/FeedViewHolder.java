70058e3094ad7194c6bcdd81fa5b4ecb3358fc7b
Harrison <tranharrywich@gmail.com>
2017-07-31 18:44:18 -0700

    Made the no image available smaller.

--- app/src/main/java/us/wetpaws/wydlist/viewholder/FeedViewHolder.java
+++ app/src/main/java/us/wetpaws/wydlist/viewholder/FeedViewHolder.java
@@ -41,8 +41,8 @@ public class FeedViewHolder extends RecyclerView.ViewHolder {
             GlideUtil.loadProfileIcon(url, bucketlist_bg_image);
         } else {
             String fileName = "android.resource://us.wetpaws.wydlist/" + R.drawable.no_image_available;
+            bucketlist_bg_image.setScaleType(ImageView.ScaleType.CENTER_CROP);
             GlideUtil.loadProfileIcon(fileName, bucketlist_bg_image);
-//            bucketlist_bg_image.setBackgroundColor(Color.parseColor("#000000"));
         }
     }
 
